2febe1735117a0868ac5d53d5324237b96250ddc
ArchiScript <arturskomarohov@gmail.com>
2023-02-20 21:15:44 +0200

    basic folder structure

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import "./assets/common_style/_index.common.scss";
+import "./components/_components.scss";
+import "./components/libs/input/_input";
+
+window.onload = function () {
+  require("/src/views/pages/ui-kit/ui-kit.js");
+};
