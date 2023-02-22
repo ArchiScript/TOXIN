65aa9f7363c3254fa9793ef1582ef19c1d67b9a4
ArchiScript <arturskomarohov@gmail.com>
2023-02-22 19:45:40 +0200

    added scss mixins, extends; added colors types from ui-kit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,6 @@
 import "../assets/common_style/_index.common.scss";
 import "./index.scss";
+
+// window.onload = function () {
+//   require("/src/pages/ui-kit/ui-kit.js");
+// };
---
+++ src/pages/ui-kit/ui-kit.js
@@ -0,0 +1,13 @@
+// window.onload = function () {
+//   require("/src/components/libs/input/_input");
+//   require("/src/assets/common_style/_index.common.scss");
+// };
+
+// import "./ui-kit.scss";
+// import "/src/components/libs/datepicker/_datepicker";
+// import "/src/components/libs/selectPlugin/_selectPluginInit";
+
+import "/src/assets/common_style/_index.common.scss";
+import "./ui-kit.scss";
+import "/src/components/blocks/blocks.scss";
+import "/src/components/elements/";
