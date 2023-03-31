bd69955da2558991eefae9d8bb7d9f0d7999000c
ArchiScript <arturskomarohov@gmail.com>
2023-03-31 12:25:48 +0300

    feat(checkbox-block): add static nonexpandable block of checkboxes with extra-styles
    add pug plugin with structure of static checkbox-list, make extra span of subtitle, style of
    label-text differs from regular checkbox

--- src/components/blocks/checkbox-list/_checkbox-list.js
+++ src/components/blocks/checkbox-list/_checkbox-list.js
@@ -2,7 +2,7 @@ const arrows = document.querySelectorAll(".checkbox-list");
 
 arrows.forEach((a) => {
   let $header = a.querySelector(".checkbox-list__header");
-  $header.addEventListener("click", function (e) {
+  $header.addEventListener("click", function () {
     a.classList.toggle("--expanded");
   });
 });
